a0bcdf689bba762c4a33147d2e16aa218d4a10c2
Niklas Weber <nweber@student.42Heilbronn.de>
2026-02-03 12:51:50 +0100

    remove debug output from Fixed class constructors and assignment operator

--- ex03/Fixed.cpp
+++ ex03/Fixed.cpp
@@ -1,32 +1,23 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() : _raw(0) {
-  std::cout << "Defaul constructor called" << std::endl;
-}
+Fixed::Fixed() : _raw(0) {}
 
-Fixed::Fixed(const int n) : _raw(n << _fb) {
-  std::cout << "Int constructor called" << std::endl;
-}
+Fixed::Fixed(const int n) : _raw(n << _fb) {}
 
-Fixed::Fixed(const float f) : _raw(static_cast<int>(roundf(f * (1 << _fb)))) {
-  std::cout << "Float constructor called" << std::endl;
-}
+Fixed::Fixed(const float f) : _raw(static_cast<int>(roundf(f * (1 << _fb)))) {}
 
 
 Fixed::Fixed(const Fixed& other) {
-  std::cout << "Copy constructor called" << std::endl;
   _raw = other.getRawBits();
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
-  std::cout << "Copy assignment operator called" << std::endl;
   if (this != &other)
     _raw = other.getRawBits();
   return *this;
 }
 
 Fixed::~Fixed() {
-  std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const {
